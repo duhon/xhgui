c0da3d3a2e7b08008522d71e3e4bf315ebe9a1e9
AVoskoboinikov <avoskoboinikov@magento.com>
2019-03-26 17:47:46 +0100

    fix for a review

--- webroot/js/request-params.js
+++ webroot/js/request-params.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('#view-request-params').click(function () {
         $('#request-params-details').addClass('active');
         $('#params-json-viwer').jsonViewer(JSON.parse($('.request-params-details-content').attr('data-params')));
-    })
+    });
 
     $('#request-params-details').find('.request-params-button-close').on('click', function() {
         $('#request-params-details').removeClass('active');
