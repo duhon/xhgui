a2616b10812b17080ded7f19dc6e35c789c64d85
AVoskoboinikov <avoskoboinikov@magento.com>
2019-03-27 22:17:53 +0100

    Add params to listing

--- webroot/js/request-params.js
+++ webroot/js/request-params.js
@@ -1,12 +1,13 @@
 // Utilitarian DOM behavior.
 $(document).ready(function () {
-    $('#view-request-params').click(function () {
+    $('.view-request-params').click(function () {
         $('#request-params-details').addClass('active');
-        $('#params-json-viwer').jsonViewer(JSON.parse($('.request-params-details-content').attr('data-params')));
+        $('#params-json-viewer').jsonViewer(JSON.parse($(this).attr('data-params')));
     });
 
     $('#request-params-details').find('.request-params-button-close').on('click', function() {
         $('#request-params-details').removeClass('active');
+        $('#params-json-viewer').html("");
         return false;
     });
 });
